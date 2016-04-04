f5fefd5b3b727bfc25d3eb20fa6bd028242eec2f
Bethany Eastman <bethanyeastman@gmail.com>
2016-04-04 14:40:48 -0700

    I added another activity called SomeOtherActivity as well as added Log messages in another activity

--- app/src/main/java/edu/uw/tacoma/mmuppa/activitylifecycle/AnotherActivity.java
+++ app/src/main/java/edu/uw/tacoma/mmuppa/activitylifecycle/AnotherActivity.java
@@ -2,13 +2,17 @@ package edu.uw.tacoma.mmuppa.activitylifecycle;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class AnotherActivity extends Activity {
+
+    private final String TAG = "AnotherActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_another);
+        Log.i(TAG, "OnCreate method called");
     }
 
 }
